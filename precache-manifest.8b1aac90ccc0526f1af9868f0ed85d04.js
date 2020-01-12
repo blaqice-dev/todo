857b90ca824ecf5996b88e69d343d1524cc78d56
Blaq Ice <blaqice0@gmail.com>
2020-01-12 03:28:10 +0000

    Add files via upload

---
+++ precache-manifest.8b1aac90ccc0526f1af9868f0ed85d04.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a784de85d78d132b39f74f33d518fbc",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "ec7dffc604bd5a910d2f",
+    "url": "/todo/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "4914b6c96bab39f0e72e",
+    "url": "/todo/static/js/2.a20cf6af.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/todo/static/js/2.a20cf6af.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ec7dffc604bd5a910d2f",
+    "url": "/todo/static/js/main.50fa2d43.chunk.js"
+  },
+  {
+    "revision": "923c1c3453704e07cde6",
+    "url": "/todo/static/js/runtime-main.3ff6d00c.js"
+  }
+]);
